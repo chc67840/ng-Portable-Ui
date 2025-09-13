88fb6b893eadcde093c4598f519f0a444e593c22
chc67840 <chc67840@gmail.com>
2025-09-13 15:05:08 +0530

    chore: cleanup duplicates (rename ds-custom doc, remove extra tailwind config & unused styles.css)

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** Tailwind CSS Config (ESM) duplicated from tailwind.config.cjs so Angular builder auto-detects */
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './src/**/*.{html,ts}'
-    ],
-    theme: { extend: {} },
-    plugins: []
-};
